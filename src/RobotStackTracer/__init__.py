26176bf304307316740362db938a3b0fac7e234a
Mikko Korpela <mikko.korpela@gmail.com>
2021-07-10 10:06:05 +0300

    fix variable resolving too early

--- src/RobotStackTracer/__init__.py
+++ src/RobotStackTracer/__init__.py
@@ -13,13 +13,14 @@
 # limitations under the License.
 
 from enum import IntEnum
+from functools import cached_property
 from os import path
 
 from robot.errors import VariableError
 from robot.libraries.BuiltIn import BuiltIn
 from robot.utils import cut_long_message
 
-__version__ = '0.4.0'
+__version__ = "0.4.0"
 
 bi = BuiltIn()
 
@@ -40,19 +41,29 @@ class Kind(IntEnum):
 
 class StackElement:
     def __init__(
-        self, file, source, lineno, name, args=None, res_args=None, kind: Kind = Kind.Keyword
+        self, file, source, lineno, name, args=None, kind: Kind = Kind.Keyword
     ):
         self.file = file
         self.source = source
         self.lineno = lineno
         self.name = name
         self.args = args or []
-        self.resolved_args = res_args or {}
         self.kind = kind
+
+    @cached_property
+    def resolved_args(self):
+        res_args = {}
+        for arg in self.args:
+            try:
+                resolved = bi.replace_variables(arg)
+                if resolved != arg:
+                    res_args[str(arg)] = f"{resolved} ({type(resolved).__name__})"
+            except VariableError:
+                res_args[str(arg)] = "<Unable to define variable value>"
+        return res_args
 
 
 class RobotStackTracer:
-
     ROBOT_LISTENER_API_VERSION = 2
 
     def __init__(self):
@@ -67,57 +78,54 @@ class RobotStackTracer:
         self.SuiteTrace.append(attrs["source"])
 
     def library_import(self, name, attrs):
-        self.lib_files[name] = attrs.get('source')
+        self.lib_files[name] = attrs.get("source")
 
     def resource_import(self, name, attrs):
-        self.lib_files[name] = attrs.get('source')
+        self.lib_files[name] = attrs.get("source")
 
     def start_test(self, name, attrs):
         self.StackTrace = [
             StackElement(
-                self.SuiteTrace[-1], self.SuiteTrace[-1], attrs["lineno"], name, kind=Kind.Test
+                self.SuiteTrace[-1],
+                self.SuiteTrace[-1],
+                attrs["lineno"],
+                name,
+                kind=Kind.Test,
             )
         ]
 
     def start_keyword(self, name, attrs):
         source = attrs.get(
-            'source', self.StackTrace[-1].file if self.StackTrace else self.SuiteTrace[-1]
+            "source",
+            self.StackTrace[-1].file if self.StackTrace else self.SuiteTrace[-1],
         )
-        file = self.lib_files.get(attrs.get('libname'), source)
+        file = self.lib_files.get(attrs.get("libname"), source)
 
         self.StackTrace.append(
             StackElement(
                 file,
                 self.fix_source(source),
-                attrs.get('lineno', None),
-                attrs['kwname'],
-                attrs['args'],
-                self.resolve_variables(attrs),
+                attrs.get("lineno", None),
+                attrs["kwname"],
+                attrs["args"],
             )
         )
-        if attrs['kwname'] in muting_keywords:
-            self.mutings.append(attrs['kwname'])
+        if attrs["kwname"] in muting_keywords:
+            self.mutings.append(attrs["kwname"])
         self.new_error = True
 
     def fix_source(self, source):
-        if source and path.isdir(source) and path.isfile(path.join(source, '__init__.robot')):
-            return path.join(source, '__init__.robot')
+        if (
+            source
+            and path.isdir(source)
+            and path.isfile(path.join(source, "__init__.robot"))
+        ):
+            return path.join(source, "__init__.robot")
         else:
             return source
 
-    def resolve_variables(self, attrs):
-        res_args = {}
-        for arg in attrs["args"]:
-            try:
-                resolved = bi.replace_variables(arg)
-                if resolved != arg:
-                    res_args[str(arg)] = f'{resolved} ({type(resolved).__name__})'
-            except VariableError:
-                res_args[str(arg)] = '<Variable does not yet exist.>'
-        return res_args
-
     def end_keyword(self, name, attrs):
-        if self.mutings and attrs['kwname'] == self.mutings[-1]:
+        if self.mutings and attrs["kwname"] == self.mutings[-1]:
             self.mutings.pop()
         if attrs["status"] == "FAIL" and self.new_error and not self.mutings:
             print("\n".join(self._create_stacktrace_text()))
@@ -125,7 +133,7 @@ class RobotStackTracer:
         self.new_error = False
 
     def _create_stacktrace_text(self) -> str:
-        error_text = [f'  ']
+        error_text = [f"  "]
         error_text += ["  Traceback (most recent call last):"]
         call: StackElement
         for index, call in enumerate(self.StackTrace):
@@ -137,10 +145,12 @@ class RobotStackTracer:
                     else f"{call.source}:0"
                 )
                 error_text += [f'    {"~" * 74}']
-                error_text += [f'    File  {path}']
-                error_text += [f'    {kind}  {call.name}    {"    ".join(call.args or [])}']
+                error_text += [f"    File  {path}"]
+                error_text += [
+                    f'    {kind}  {call.name}    {"    ".join(call.args or [])}'
+                ]
                 for var, value in call.resolved_args.items():
-                    error_text += [f'      |  {var} = {cut_long_message(value)}']
+                    error_text += [f"      |  {var} = {cut_long_message(value)}"]
         error_text += [f'{"_" * 78}']
         return error_text
 
